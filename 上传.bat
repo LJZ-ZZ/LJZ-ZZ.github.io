b58730292f070632799b1886f44f92c84e473bfa
LJZ-ZZ <1780109923@qq.com>
2023-01-22 19:49:17 +0800

    修改

--- 上传.bat
+++ 上传.bat
@@ -1,8 +1,10 @@
 git config --global user.email "1780109923@qq.com"
 git config --global user.name "LJZ-ZZ"
 ::git clone https://github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
+git init
 git add .
 git status
 git commit -m "ÐÞ¸Ä"
+git remote add origin https://github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
 git push -u origin master
 pause
