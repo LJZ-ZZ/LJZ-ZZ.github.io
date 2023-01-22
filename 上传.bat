1a17aaf96d946e53a6c2513cf97e8d09bfd6e406
LJZ-ZZ <1780109923@qq.com>
2023-01-22 23:43:24 +0800

    修改

--- 上传.bat
+++ 上传.bat
@@ -1,6 +1,6 @@
 git config --global user.email "1780109923@qq.com"
 git config --global user.name "LJZ-ZZ"
-git clone https://github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
+::git clone https://github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
 git init
 git add .
 git status
