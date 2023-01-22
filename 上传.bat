8368ec618be372f3e4374170c7cdff8f7171db6d
LJZ-ZZ <1780109923@qq.com>
2023-01-22 18:32:49 +0800

    修改

---
+++ 上传.bat
@@ -0,0 +1,8 @@
+git config --global user.email "1780109923@qq.com"
+git config --global user.name "LJZ-ZZ"
+git clone https://github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
+git add .
+git status
+git commit -m "ÐÞ¸Ä"
+git push
+pause
