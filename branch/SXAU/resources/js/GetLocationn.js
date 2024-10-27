16be3c8a491eae6faa96a776aee56b841b9412d2
LJZ-ZZ <1780109923@qq.com>
2024-10-27 23:32:55 +0800

    修改

--- branch/SXAU/resources/js/GetLocationn.js
+++ branch/SXAU/resources/js/GetLocationn.js
@@ -1,9 +1,12 @@
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-    });
-  } else {
-    console.log("Geolocation is not supported by this browser.");
-  }
+$(function(){
+    $("table tr:nth-child(even)").addClass("even");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+})
