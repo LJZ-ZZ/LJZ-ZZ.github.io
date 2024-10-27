dff5097dad21af17e24ddfb1573bf24a6074b18b
LJZ-ZZ <1780109923@qq.com>
2024-10-27 23:34:01 +0800

    修改

--- branch/SXAU/resources/js/iframe.js
+++ branch/SXAU/resources/js/iframe.js
@@ -2,7 +2,7 @@
 //     $("table tr:nth-child(even)").addClass("even");
     
 // })
-function(){
+function iframe0(){
     var iframe = document.getElementById('iframe0');
     iframe.src = 'https://ljz-zz.github.io/branch/SXAU/resources/others/money.htm';
 
