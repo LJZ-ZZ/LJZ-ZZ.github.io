357938431c3926f0e9ad86f0a9ef06fd3206b477
LJZ-ZZ <1780109923@qq.com>
2024-10-27 23:24:20 +0800

    修改

--- branch/SXAU/resources/js/iframe.js
+++ branch/SXAU/resources/js/iframe.js
@@ -3,4 +3,5 @@ $(function(){
     var iframe = document.getElementById('iframe0');
     iframe.src = 'https://ljz-zz.github.io/branch/SXAU/resources/others/money.htm';
 
+    p0 = document.getElementById("p0").innerHTML = " ";
 })
