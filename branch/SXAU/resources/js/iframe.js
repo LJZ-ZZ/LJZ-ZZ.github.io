02e9dd43fd56b969e904cdda12f4eb05aea0b7b3
终临与渊 <69313011+LJZ-ZZ@users.noreply.github.com>
2024-10-28 10:14:54 +0800

    Update iframe.js

--- branch/SXAU/resources/js/iframe.js
+++ branch/SXAU/resources/js/iframe.js
@@ -1,7 +1,11 @@
-// $(function(){
-//     $("table tr:nth-child(even)").addClass("even");
-    
-// })
+$(function(){
+    $("table tr:nth-child(even)").addClass("even");
+    var iframe = document.getElementById('iframe0');
+    iframe.src = 'https://ljz-zz.github.io/branch/SXAU/resources/others/money.htm';
+
+    p0 = document.getElementById("p0").innerHTML = " ";
+
+})
 function iframe0(){
     var iframe = document.getElementById('iframe0');
     iframe.src = 'https://ljz-zz.github.io/branch/SXAU/resources/others/money.htm';
