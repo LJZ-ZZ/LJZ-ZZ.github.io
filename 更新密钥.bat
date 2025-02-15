acb4a22f70631ff60bda6f93913301ada2ab54a4
LJZ-ZZ <1780109923@qq.com>
2025-02-15 11:07:01 +0800

    修改

--- 更新密钥.bat
+++ 更新密钥.bat
@@ -1,2 +1,4 @@
 git remote rm origin
-git remote add origin https://新的密钥@github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
+::git remote add origin https://新的密钥@github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
+git remote add origin https://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@github.com/LJZ-ZZ/LJZ-ZZ.github.io.git
+pause
